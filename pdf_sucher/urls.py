f46e3edb7085abb114e7148f1faccd902a35a443
taras.yuzkiv <taras.yuzkiv@schuch.de>
2025-06-16 12:45:37 +0200

    alles geht!!!

--- pdf_sucher/urls.py
+++ pdf_sucher/urls.py
@@ -6,11 +6,10 @@ from . import views
 app_name = 'pdf_sucher'
 
 urlpatterns = [
-    # KORREKTUR: Der Name wurde auf 'pdf_suche' zurückgeändert, um konsistent zu sein.
-    # Die Templates (base.html, startseite.html) sollten {% url 'pdf_sucher:pdf_suche' %} verwenden.
     path('suche/', views.pdf_suche, name='pdf_suche'),
 
-    # KORREKTUR: Der Pfad wurde angepasst, damit er zum Aufruf im Template passt.
+    # KORREKTUR: Das überflüssige '/page/' wurde aus dem Pfad entfernt,
+    # damit es zum Aufruf im Template passt.
     path('preview/<str:filename>/<int:page_num>/', views.pdf_page_preview, name='pdf_page_preview'),
 
     path('view/<str:filename>/', views.view_pdf, name='view_pdf'),
